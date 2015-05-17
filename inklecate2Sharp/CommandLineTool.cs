4cdda8f27f2b7be776779c9f01d90b98537ba40e
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2015-05-17 18:16:57 +0100

    Parser.cs initial, starting to make bare bones string parsing functions, CommandLineTool.cs initial (based on Program.cs)

---
+++ inklecate2Sharp/CommandLineTool.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+
+namespace inklecate2Sharp
+{
+	class CommandLineTool
+	{
+		struct Options {
+			public bool testMode;
+			public string inputFile;
+		}
+
+		public static void Main (string[] args)
+		{
+			new CommandLineTool(args);
+		}
+
+		CommandLineTool(string[] args)
+		{
+			Options opts = ProcessArguments(args);
+
+			string inputString = File.ReadAllText(opts.inputFile);
+
+			Parser parser = new Parser(inputString);
+			parser.Parse();
+		}
+
+		Options ProcessArguments(string[] args)
+		{
+			Options opts = new Options();
+
+			// Process arguments
+			foreach (string arg in args) {
+
+				// Options
+				var firstChar = arg.Substring(0,1);
+				if (firstChar == "-" && arg.Length > 1) {
+
+					var secondChar = arg.Substring(1,1);
+
+					// Test mode
+					if (secondChar == "t") {
+						opts.testMode = true;
+						opts.inputFile = "test.ink";
+					}
+				}
+			}
+
+			return opts;
+		}
+
+	}
+}
---
+++ inklecate2Sharp/Parser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace inklecate2Sharp
+{
+	public class Parser
+	{
+		public string inputString { get; }
+
+		private char[] _chars;
+		private int _index;
+
+		public Parser (string str)
+		{
+			_chars = str.ToCharArray ();
+			
+			inputString = str;
+		}
+
+		public void Parse()
+		{
+			ParseString ("Hello");
+			ParseString (" ");
+			ParseString ("world");
+		}
+
+		public bool ParseString(string str)
+		{
+			if (str.Length > remainingLength) {
+				return false;
+			}
+
+			int oldIndex = _index;
+
+			bool success = true;
+			foreach (char c in str) {
+				if ( _chars[_index] != c) {
+					success = false;
+					break;
+				}
+				_index++;
+			}
+
+			if (!success) {
+				_index = oldIndex;
+			}
+
+			return success;
+		}
+
+		public string remainingString
+		{
+			get {
+				return new string(_chars, _index, remainingLength);
+			}
+		}
+
+		public int remainingLength
+		{
+			get {
+				return _chars.Length - _index;
+			}
+		}
+	}
+}
+
