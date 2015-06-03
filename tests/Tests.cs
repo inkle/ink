f888d7e3f9a24b2c59ef89b804c89dcd0a678ca5
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2015-06-03 20:37:39 +0100

    Test for conditionals

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -85,6 +85,42 @@ namespace Tests
             Assert.AreEqual (story.currentText, "Hello, world.\n");
         }
 
+        [Test ()]
+        public void TestConditionals()
+        {
+            var storyStr =
+                @"
+{false:not true|true}
+{
+   - 4 > 5: not true
+   - 5 > 4: true
+}
+{ 2*2 > 3:
+   - true
+   - not true
+}
+{
+   - 1 > 3: not true
+   - { 2+2 == 4:
+        - true
+        - not true
+   }
+}
+{ 2*3:
+   - 1+7: not true
+   - 9: not true
+   - 1+1+1+3: true
+   - 9-3: also true but not printed
+}
+                ";
+
+            Story story = CompileString (storyStr);
+            story.Begin ();
+
+            // TODO: Fix whitespace!
+            Assert.AreEqual (story.currentText, "true\ntrue\n\ntrue\n\ntrue\n\n\ntrue\n\n");
+        }
+
 		//------------------------------------------------------------------------
 
 		[Test ()]
