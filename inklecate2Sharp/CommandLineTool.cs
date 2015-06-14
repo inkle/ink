6b0e428e656b1adb86a22a64d7bd45917ba6ffe2
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2015-06-14 10:31:03 +0100

    Stress test for timing compiler with large quantities of content

--- inklecate2Sharp/CommandLineTool.cs
+++ inklecate2Sharp/CommandLineTool.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Linq;
+using System.Diagnostics;
 
 namespace Inklewriter
 {
@@ -9,6 +10,8 @@ namespace Inklewriter
 	{
 		class Options {
 			public bool testMode;
+            public bool stressTest;
+            public bool verbose;
 			public bool playMode;
 			public string inputFile;
 		}
@@ -19,35 +22,63 @@ namespace Inklewriter
 		{
 			new CommandLineTool(args);
 		}
-
+            
 		CommandLineTool(string[] args)
 		{
-            Options opts;
-
-            if (ProcessArguments (args, out opts) == false) {
+            if (ProcessArguments (args) == false) {
                 Console.WriteLine (
-                    "Usage: inklecate2 [-p] <ink file> \n"+
-                    "   -p:    Play mode");
+                    "Usage: inklecate2 <options> <ink file> \n"+
+                    "   -p:    Play mode"+
+                    "   -v:    Verbose mode - print compilation timings"+
+                    "   -t:    Test mode - loads up test.ink"+
+                    "   -s:    Stress test mode - generates test content and times compilation");
                 Environment.Exit (ExitCodeError);
             }
 
             string inputString = null;
-            try {
-                inputString = File.ReadAllText(opts.inputFile);
-            }
-            catch {
-                Console.WriteLine ("Could not open file '" + opts.inputFile+"'");
-                Environment.Exit (ExitCodeError);
-            }
-			
 
-            InkParser parser = new InkParser(inputString, opts.inputFile);
-			Parsed.Story parsedStory = parser.Parse();
+            if (opts.stressTest) {
+
+                StressTestContentGenerator stressTestContent = null;
+                TimeOperation ("Generating test content", () => {
+                    stressTestContent = new StressTestContentGenerator (800);
+                });
+
+                Console.WriteLine ("Generated ~{0}k of test ink", stressTestContent.sizeInKiloChars);
+
+                inputString = stressTestContent.content;
+
+            } else {
+                try {
+                    inputString = File.ReadAllText(opts.inputFile);
+                }
+                catch {
+                    Console.WriteLine ("Could not open file '" + opts.inputFile+"'");
+                    Environment.Exit (ExitCodeError);
+                }
+            }
+
+            InkParser parser = null;
+            Parsed.Story parsedStory = null;
+            Runtime.Story story = null;
+
+            TimeOperation ("Creating parser", () => {
+                parser = new InkParser (inputString, opts.inputFile);
+            });
+
+            TimeOperation ("Parsing", () => {
+                parsedStory = parser.Parse();
+            });
+
             if (parsedStory == null) {
-                Environment.Exit (ExitCodeError);
+                Environment
+                    .Exit (ExitCodeError);
             }
 
-			Runtime.Story story = parsedStory.ExportRuntime ();
+            TimeOperation ("Exporting runtime", () => {
+                story = parsedStory.ExportRuntime ();
+            });
+
 			if (story == null) {
 				Environment.Exit (ExitCodeError);
 			}
@@ -61,7 +92,7 @@ namespace Inklewriter
 			}
 		}
 
-        bool ProcessArguments(string[] args, out Options opts)
+        bool ProcessArguments(string[] args)
 		{
             if (args.Length < 1) {
                 opts = null;
@@ -77,20 +108,31 @@ namespace Inklewriter
 				var firstChar = arg.Substring(0,1);
 				if (firstChar == "-" && arg.Length > 1) {
 
-					char secondChar = arg [1];
+                    for (int i = 1; i < arg.Length; ++i) {
+                        char argChar = arg [i];
 
-					switch (secondChar) {
-					case 't':
-						opts.testMode = true;
-						opts.inputFile = "test.ink";
-						break;
-					case 'p':
-						opts.playMode = true;
-						break;
-					default:
-						Console.WriteLine ("Unsupported argument type: '{0}'", secondChar);
-						break;
-					}
+                        switch (argChar) {
+                        case 't':
+                            opts.testMode = true;
+                            opts.inputFile = "test.ink";
+                            break;
+                        case 's':
+                            opts.testMode = true;
+                            opts.stressTest = true;
+                            opts.verbose = true;
+                            break;
+                        case 'p':
+                            opts.playMode = true;
+                            break;
+                        case 'v':
+                            opts.verbose = true;
+                            break;
+                        default:
+                            Console.WriteLine ("Unsupported argument type: '{0}'", argChar);
+                            break;
+                        }
+                    }
+					
 				}
 			}
 
@@ -101,5 +143,28 @@ namespace Inklewriter
 			return true;
 		}
 
+        void TimeOperation(string opDescription, Action op)
+        {
+            if (!opts.verbose) {
+                op ();
+                return;
+            }
+
+            Console.WriteLine ("{0}...", opDescription);
+
+            var stopwatch = Stopwatch.StartNew ();
+            op ();
+            stopwatch.Stop ();
+
+            long duration = stopwatch.ElapsedMilliseconds;
+
+            if (duration > 500) {
+                Console.WriteLine ("{0} took {1}s", opDescription, duration / 1000.0f);  
+            } else {
+                Console.WriteLine ("{0} took {1}ms", opDescription, duration);  
+            }
+        }
+
+        Options opts;
 	}
 }
---
+++ inklecate2Sharp/StressTestContentGenerator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+
+namespace Inklewriter
+{
+    public class StressTestContentGenerator
+    {
+        public string content { get; }
+        public int sizeInKiloChars { get { return content.Length / 1024; } }
+
+        public StressTestContentGenerator (int repetitions)
+        {
+            var initialContent = "~ var myVar = 5 + 5 + (3 * 2 * 9) + 1.2\n\n==> test1\n\n";
+
+            var repeatingContent = @"== test__X__ ==
+This is some content in a test knot with the number {myVar}.
+{ myVar > 2:
+    - This evaluation is true.
+    - This evaluation is false.
+}
+This is some more content.
+ * A choice
+    * * A sub-choice ->
+ * Another choice ==> somewhere_else__X__
+ - A gather
+ Some more content within the gather. Some more content within the gather. Some more content within the gather. Some more content within the gather. Some more content within the gather. 
+ Some more content within the gather. Some more content within the gather. Some more content within the gather. 
+ myVar is {myVar}.
+ * Another choice[.] which is obviously really cool
+    And some content within that choice
+    * * Another sub choice
+     * * * Another another sub choice
+      * * * * Yet another choice.
+              With more content in that choice.
+ - A final gather.
+ Nice one.
+ Isn't this great?
+ ==> test__Y__
+ ~ done
+
+
+== somewhere_else__X__ ==
+ This is somewhere else
+ ~ done
+
+";
+
+            var sb = new StringBuilder ();
+            sb.Append (initialContent);
+
+            for (int i = 1; i <= repetitions; ++i) {
+
+                var content = repeatingContent.Replace ("__X__", i.ToString());
+                content = content.Replace ("__Y__", (i + 1).ToString());
+
+                sb.Append (content);
+            }
+
+            var finalContent = @" == test__X__
+    Done!
+    ~ done";
+
+            finalContent = finalContent.Replace ("__X__", (repetitions+1).ToString ());
+            sb.AppendFormat (finalContent, repetitions.ToString());
+
+            this.content = sb.ToString ();
+        }
+    }
+}
+
