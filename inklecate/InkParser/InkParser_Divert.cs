b609007291526d9cdfd565fe46591f2f499c43a1
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2015-10-07 12:05:59 +0100

    Kill dead code

--- inklecate/InkParser/InkParser_Divert.cs
+++ inklecate/InkParser/InkParser_Divert.cs
@@ -10,7 +10,6 @@ namespace Inklewriter
         const string stitchDivertArrow = "=>";
         const string weavePointDivertArrow = "->";
         const string weavePointDivertAltArrow = "-->";
-        const string weavePointExplicitGather = "<explicit-gather>";
 
         protected Divert Divert()
         {
@@ -65,38 +64,6 @@ namespace Inklewriter
         List<string> DotSeparatedDivertPathComponents()
         {
             return Interleave<string> (Spaced (Identifier), Exclude (String (".")));
-        }
-
-        string ParseDivertTargetWithArrow(string arrowStr)
-        {
-            Whitespace ();
-
-            string parsedArrowResult = ParseString (arrowStr);
-
-            // Allow both -> and --> for weaves
-            if (parsedArrowResult == null && arrowStr == weavePointDivertArrow) {
-                parsedArrowResult = ParseString (weavePointDivertAltArrow);
-            }
-
-            if (parsedArrowResult == null) {
-                return null;
-            }
-
-            Whitespace ();
-
-            string targetName = null;
-
-            // Weave arrows without a target mean "explicit gather"
-            if (arrowStr == weavePointDivertArrow) {
-                targetName = Identifier ();
-                if (targetName == null) {
-                    targetName = weavePointExplicitGather;
-                }
-            } else {
-                targetName = (string) Expect(Identifier, "name of target to divert to");
-            }
-
-            return targetName;
         }
 
         protected string DivertArrow()
