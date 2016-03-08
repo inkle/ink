e846072319823aa76b2d944fc229720bbd165a42
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2016-03-08 10:46:24 +0000

    Include print_num in tests, since it's a tough/recursive test of various glues

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -1940,6 +1940,78 @@ CONST kX = ""hi""
             Assert.AreEqual("hi\n", story.Continue ());
         }
 
+        [Test ()]
+        public void TestPrintNum()
+        {
+            var story = CompileString (@"
+. {print_num(4)} .
+. {print_num(15)} .
+. {print_num(37)} .
+. {print_num(101)} .
+. {print_num(222)} .
+. {print_num(1234)} .
+
+=== function print_num(x) ===
+{ 
+    - x >= 1000:
+        {print_num(x / 1000)} thousand { x mod 1000 > 0:{print_num(x mod 1000)}}
+    - x >= 100:
+        {print_num(x / 100)} hundred { x mod 100 > 0:and {print_num(x mod 100)}}
+    - x == 0:
+        zero
+    - else:
+        { x >= 20:
+            { x / 10:
+                - 2: twenty
+                - 3: thirty
+                - 4: forty
+                - 5: fifty
+                - 6: sixty
+                - 7: seventy
+                - 8: eighty
+                - 9: ninety
+            }
+            { x mod 10 > 0:<>-<>}
+        }
+        { x < 10 || x > 20:
+            { x mod 10:
+                - 1: one
+                - 2: two
+                - 3: three
+                - 4: four        
+                - 5: five
+                - 6: six
+                - 7: seven
+                - 8: eight
+                - 9: nine
+            }
+        - else:     
+            { x:
+                - 10: ten
+                - 11: eleven       
+                - 12: twelve
+                - 13: thirteen
+                - 14: fourteen
+                - 15: fifteen
+                - 16: sixteen      
+                - 17: seventeen
+                - 18: eighteen
+                - 19: nineteen
+            }
+        }
+}
+");
+
+            Assert.AreEqual(
+@". four .
+. fifteen .
+. thirty-seven .
+. one hundred and one .
+. two hundred and twenty-two .
+. one thousand two hundred and thirty-four .
+", story.ContinueMaximally ());
+        }
+
 		//------------------------------------------------------------------------
 
 		[Test ()]
