db9789b5fdb1f05fa845e5c556ffabec0bd95725
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2016-03-17 08:23:28 -0700

    *Slightly* more useful error message for exception we've had a couple of times before... need to work out the commonality between them to make it more useful.

--- ink-engine-runtime/VariablesState.cs
+++ ink-engine-runtime/VariablesState.cs
@@ -122,7 +122,7 @@ namespace Ink.Runtime
             varValue = _callStack.GetTemporaryVariableWithName (name, contextIndex);
 
             if (varValue == null)
-                throw new System.Exception ("Shouldn't ever have a null value where it couldn't be found at all");
+                throw new System.Exception ("Runtime error: Variable '"+name+"' could not be found in context '"+contextIndex+"'. This shouldn't be possible so is a bug in the ink engine. Please try to construct a minimal story that reproduces the problem and report to inkle, thank you!");
 
             return varValue;
         }
