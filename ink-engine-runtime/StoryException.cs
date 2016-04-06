cf1a15e36e8f8c1cb594104cf5f9d06951d3e95a
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2016-04-06 08:46:44 +0100

    Made StoryExeption public

--- ink-engine-runtime/StoryException.cs
+++ ink-engine-runtime/StoryException.cs
@@ -1,10 +1,23 @@
 ﻿namespace Ink.Runtime
 {
-    internal class StoryException : System.Exception
+    /// <summary>
+    /// Exception that represents an error when running a Story at runtime.
+    /// An exception being thrown of this type is typically when there's
+    /// a bug in your ink, rather than in the ink engine itself!
+    /// </summary>
+    public class StoryException : System.Exception
     {
-        public bool useEndLineNumber;
+        internal bool useEndLineNumber;
 
+        /// <summary>
+        /// Constructs a default instance of a StoryException without a message.
+        /// </summary>
         public StoryException () { }
+
+        /// <summary>
+        /// Constructs an instance of a StoryException with a message.
+        /// </summary>
+        /// <param name="message">The error message.</param>
         public StoryException(string message) : base(message) {}
     }
 }
