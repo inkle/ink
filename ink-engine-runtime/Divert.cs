eeb83514db55166a6d6b124cb492e07b4d5eab3e
mwchase <max.chase@gmail.com>
2016-05-01 17:34:29 -0400

    Invalidate cache of targetContent
    targetContent caches the result of resolving _targetPath, but the cache never invalidates, so targetContent, and in some cases, targetPath, can be stale. This change invalidates the cache when _targetPath changes.

--- ink-engine-runtime/Divert.cs
+++ ink-engine-runtime/Divert.cs
@@ -17,6 +17,7 @@ namespace Ink.Runtime
             }
             set {
                 _targetPath = value;
+                _targetContent = null;
             } 
         }
         Path _targetPath;
