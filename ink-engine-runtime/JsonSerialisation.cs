63609311a412ddc32dce90b90fae887e324d9dde
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2016-09-11 18:53:10 +0100

    Bah stupid me for pushing before running unit tests. Fixed!

--- ink-engine-runtime/JsonSerialisation.cs
+++ ink-engine-runtime/JsonSerialisation.cs
@@ -544,6 +544,8 @@ namespace Ink.Runtime
             _controlCommandNames [(int)ControlCommand.CommandType.NoOp] = "nop";
             _controlCommandNames [(int)ControlCommand.CommandType.ChoiceCount] = "choiceCnt";
             _controlCommandNames [(int)ControlCommand.CommandType.TurnsSince] = "turns";
+            _controlCommandNames [(int)ControlCommand.CommandType.Random] = "rnd";
+            _controlCommandNames [(int)ControlCommand.CommandType.SeedRandom] = "srnd";
             _controlCommandNames [(int)ControlCommand.CommandType.VisitIndex] = "visit";
             _controlCommandNames [(int)ControlCommand.CommandType.SequenceShuffleIndex] = "seq";
             _controlCommandNames [(int)ControlCommand.CommandType.StartThread] = "thread";
