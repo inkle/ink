3b53e460d5099cb9c3d4b375a16d92e55b88dc14
Ivaylo <ivaylo5ev@gmail.com>
2017-07-14 13:47:45 +0300

    tabs to spaces fix

--- inklecate/ParsedHierarchy/IncludedFile.cs
+++ inklecate/ParsedHierarchy/IncludedFile.cs
@@ -3,7 +3,7 @@ namespace Ink.Parsed
 {
     internal class IncludedFile : Parsed.Object
     {
-		public Parsed.Story includedStory { get; private set; }
+        public Parsed.Story includedStory { get; private set; }
 
         public IncludedFile (Parsed.Story includedStory)
         {
@@ -16,20 +16,20 @@ namespace Ink.Parsed
             return null;
         }
     }
-	internal class CharacterRange : Parsed.Object
-	{
-		public string rangeName { get; private set; }
+    internal class CharacterRange : Parsed.Object
+    {
+        public string rangeName { get; private set; }
 
-		public CharacterRange (string rangeName)
-		{
-			this.rangeName = rangeName;
-		}
+        public CharacterRange (string rangeName)
+        {
+            this.rangeName = rangeName;
+        }
 
-		public override Runtime.Object GenerateRuntimeObject ()
-		{
-			// Left to the main story to process
-			return null;
-		}
-	}
+        public override Runtime.Object GenerateRuntimeObject ()
+        {
+            // Left to the main story to process
+            return null;
+        }
+    }
 }
 
