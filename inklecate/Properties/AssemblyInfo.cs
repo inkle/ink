b0ae63edc36f25585b8d23a8b977323a55ea0fce
13xforever <ilya_veselov@kontur.ru>
2019-02-26 20:30:31 +0500

    updated project files to use new .net core format
    removed binary nuget (replaced with the built-in `dotnet restore`)
    removed most of `AssemblyInfo.cs` content (these are generated automatically now from the project properties)
    removed `ink-engine-nuget` project (replaced with built-in option to create package on build, or with explicit `dotnet pack` command)
    removed `.shproj` and `.projitems` files from `ink-engine-runtime` (not sure what they are, but seem to be obsolete)
    updated config files for vs code
    updated travis build script
    updated nunit to version 3 for compatibility

--- inklecate/Properties/AssemblyInfo.cs
+++ inklecate/Properties/AssemblyInfo.cs
@@ -1,29 +1,4 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
-[assembly: AssemblyTitle ("inklecate")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("inkle Ltd")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("inkle Ltd")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
 [assembly: InternalsVisibleTo("ink-tests")]
-
-[assembly: AssemblyVersion ("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
-
