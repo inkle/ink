4238ccdd0c53f8da678838d14e173ae286194fb7
Tom <tom.kail@inklestudios.com>
2019-11-19 10:13:39 +0000

    Fixes error in the profiler when pointer is null

--- ink-engine-runtime/Profiler.cs
+++ ink-engine-runtime/Profiler.cs
@@ -70,18 +70,20 @@ namespace Ink.Runtime
 
 			var stack = new string[callstack.elements.Count];
 			for(int i=0; i<stack.Length; i++) {
-				var objPath = callstack.elements[i].currentPointer.path;
-				string stackElementName = "";
+                string stackElementName = "";
+                if(!callstack.elements[i].currentPointer.isNull) {
+                    var objPath = callstack.elements[i].currentPointer.path;
 
-				for(int c=0; c<objPath.length; c++) {
-					var comp = objPath.GetComponent(c);
-					if( !comp.isIndex ) {
-						stackElementName = comp.name;
-						break;
-					}
-				}
+                    for(int c=0; c<objPath.length; c++) {
+                        var comp = objPath.GetComponent(c);
+                        if( !comp.isIndex ) {
+                            stackElementName = comp.name;
+                            break;
+                        }
+                    }
 
-				stack[i] = stackElementName;
+                }
+                stack[i] = stackElementName;
 			}
 				
 			_currStepStack = stack;
