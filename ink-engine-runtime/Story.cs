bc371246dd88a6c0233bb79c37ea2477c78e8b39
Joseph Humfrey <joseph.humfrey@inklestudios.com>
2022-08-30 15:52:49 +0200

    Bump story version before I forget! (Dynamic tags have new control commands)

--- ink-engine-runtime/Story.cs
+++ ink-engine-runtime/Story.cs
@@ -16,7 +16,7 @@ namespace Ink.Runtime
         /// <summary>
         /// The current version of the ink story file format.
         /// </summary>
-        public const int inkVersionCurrent = 20;
+        public const int inkVersionCurrent = 21;
 
         // Version numbers are for engine itself and story file, rather
         // than the story state save format
